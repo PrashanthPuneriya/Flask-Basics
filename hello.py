2b2812bea6460e197d57baa3c5c79ef7d0c5264c
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-06-18 13:36:57 +0530

    Add tests

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-from flask import Flask
-# from markupsafe import escape
+from flask import Flask, url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -13,7 +13,7 @@ def hello_world():
 
 @app.route('/profile/<string:username>/')
 def show_user_profile(username):
-    # Can access the URL without trailing slash
+    # *** Can access the URL without trailing slash ***
     return "Profile of user: %s" % username
     # return "Profile of user: %s" % escape(username)
 
@@ -23,4 +23,9 @@ def show_single_post(post_id):
 
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
-    return 'Subpath %s' % subpath
+    return 'Subpath %s' % escape(subpath)
+
+# TESTING
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('show_user_profile', username='Prashanth', next='/'))
