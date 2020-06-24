6db18d3a3fcc4eb4982be7d7616c977e3797f58c
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-06-24 13:00:45 +0530

    Use f-string

--- hello.py
+++ hello.py
@@ -1,35 +1,37 @@
 from flask import Flask, url_for, render_template
-from markupsafe import escape
+# from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/index')
+# *** CANNOT access the URL with trailing slash ***
 def index():
-    # *** CANNOT access the URL with trailing slash ***
     return "Index Page"
 
 @app.route('/home/')
+# *** CAN access the URL without trailing slash ***
 @app.route('/home/<string:username>/')
 def hello_world(username=None):
-    # *** Can access the URL without trailing slash ***
     return render_template('hello.html', username=username)
 
 @app.route('/profile/<string:username>')
 def show_user_profile(username):
-    return "Profile of user: %s" % username
-    # return "Profile of user: %s" % escape(username)
+    return f"Profile of user: {username}"
 
 @app.route('/post/<int:post_id>')
 def show_single_post(post_id):
-    return "Post ID is: %d" % post_id
+    return f"Post ID is: {post_id}"
 
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
-    return "Subpath %s" % escape(subpath)
+    return f"Subpath {subpath}"
 
-url_for('static', filename='base.css')
+# url_for('static', filename='base.css')
 
 # TESTING
 with app.test_request_context():
     print(url_for('index'))
     print(url_for('show_user_profile', username='Prashanth', next='/'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
