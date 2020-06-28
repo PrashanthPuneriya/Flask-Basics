e22cd88d9085f491ad992961ed4f735c4a76fa3c
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-06-28 22:55:28 +0530

    Demonstrate flask sessions and cookies by an example

--- myapp/example/routes.py
+++ myapp/example/routes.py
@@ -1,4 +1,4 @@
-from flask import url_for, redirect, Blueprint, current_app, g, session
+from flask import url_for, redirect, Blueprint, current_app, g, make_response, session
 
 example = Blueprint('example', __name__)
 
@@ -16,7 +16,31 @@ def index():
 @example.route('/home/')
 # *** CAN access the URL without trailing slash ***
 def home():
-    session['SESSION_TEST_PRASHANTH'] = 'prashanth-puneriya'
-    # session.clear()
-    print(session)
     return redirect(url_for("example.index"))
+
+
+@example.route('/cookies-example/')
+def cookie_example():
+    # set a cookie for 30 seconds
+    resp = make_response("See the cookies of this website")
+    resp.set_cookie('test-cookie', '123', max_age=5)
+    """
+    Accessing the cookie:-
+        request.cookies.get('key')
+    Deleting the cookie:-
+        resp.set_cookie('key', 'value', max_age=0)
+    """
+    return resp
+
+
+@example.route('/session-example/')
+def session_example():
+    if 'visits' in session:
+        session['visits'] = session.get('visits') + 1
+    else:
+        session['visits'] = 1
+    """
+    Deleting the session:-
+        session.pop('key', None)
+    """
+    return "Total visits: {}".format(session.get('visits'))
